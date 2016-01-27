5499fc812124d3a3abce64e1e1ccc4870be5389d
ronald <ronald.espinoza@peruexport-import.com>
2016-01-26 21:04:56 -0500

    se utilza event emmitter

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server=http.createServer(onRequest);
-server.listen(port,onListening);
+const server=http.createServer();
+
+server.on('request',onRequest);
+server.on('listening',onListening);
+server.listen(port);
 
 function onRequest(req,res){
 	res.end("hola io.js");
