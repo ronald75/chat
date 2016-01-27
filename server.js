d3028c662dbd6d723526988cde85f7ffd6c75544
RONALD <ronald.espinoza@peruexport-import.comgit config --global user.emailgit config --global user.email ronald.espinoza@peruexport-import.comgit config --global user.email ronald.espinoza@peruexport-import.com>
2016-01-27 13:07:06 -0500

    se definen rutas para servir archivos diferentes

--- server.js
+++ server.js
@@ -12,18 +12,43 @@ server.on('listening',onListening);
 server.listen(port);
 
 function onRequest(req,res){
-	let filename = path.join(__dirname,"public","index.html");
+	let uri = req.url;
+	if (uri.startsWith('/index')||uri==='/'){
+		return serveIndex(res);
+	}
+	if (uri==='/app.js'){
+		return serveApp(res);
+	}
+	res.statusCode = 404;
+	res.end('404 not found '+uri);
+}
+
+function serveIndex(res){
+let filename = path.join(__dirname,"public","index.html");
 
 	res.setHeader('Content-Type','text/html');
 	let rs = fs.createReadStream(filename);   // se define el stream de lectura
 	rs.pipe(res);
 	rs.on('error',function(err){
+		res.setHeader('Content-Type','text/plain');
 		rs.end(err.message);
 	})
 }
+function serveApp(res){
+	let filename = path.join(__dirname,"public","app.js");
+
+	res.setHeader('Content-Type','text/javascript');
+	let rs = fs.createReadStream(filename);   // se define el stream de lectura
+	rs.pipe(res);
+	rs.on('error',function(err){
+		res.setHeader('Content-Type','text/plain');
+		rs.end(err.message);
+	})
+
+}
 
 function onListening(){
-	console.log("el servidor esta escuchando en el puerto: "+ port);
+	console.log('el servidor esta escuchando en el puerto: '+port);
 }
 
 
